efd30fc236813ba08fa260854aca0c7707e9025e
Mayshara <mayshara@alunos.utfpr.edu.br>
2019-11-24 16:44:14 -0300

    exercicio3

--- Funcao/Exe2/exe2.c
+++ Funcao/Exe2/exe2.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <string.h>
 
+
+void data(int dia, int mes, int ano){
+    char m[20];
+
+    if(mes == 11){
+        m[20] = 'Novembro';
+    }
+
+    printf("%d  %s  %d", dia, m[20], ano);
+}
+
+
+int main(){
+    int dia, mes, ano;
+
+    printf("Dia: ");
+    scanf("%d", &dia);
+    printf("Mes: ");
+    scanf("%d", &mes);
+    printf("Ano: ");
+    scanf("%d", &ano);
+
+    data(dia,mes,ano);
+}
