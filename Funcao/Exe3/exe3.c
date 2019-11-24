bceae43da599161cb11bec83f3497cdf2917c69d
Mayshara <mayshara@alunos.utfpr.edu.br>
2019-11-24 17:03:41 -0300

    exe3

--- Funcao/Exe3/exe3.c
+++ Funcao/Exe3/exe3.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main(){
+    int n;
+
+    printf("Digite um numero: ");
+    scanf("%d", n);
+
+    verificar(n);
+}
+
+int verificar(int n){
+    if(n > 0)
+        return 1;
+
+    else if(n == 0)
+         return 0;
+
+    else
+        return -1;
+}
